2ba98593e585c572d56fe3f910b032f1c5678ae5
Vlad <fleshakvlad@yandex.ru>
2020-09-27 16:19:23 +0300

    fix login out

--- app/src/main/java/ru/zorin/messenger/fragments/auth_fragment/AuthView.kt
+++ app/src/main/java/ru/zorin/messenger/fragments/auth_fragment/AuthView.kt
@@ -2,15 +2,16 @@ package ru.zorin.messenger.fragments.auth_fragment
 
 import moxy.MvpView
 import moxy.viewstate.strategy.alias.AddToEndSingle
+import moxy.viewstate.strategy.alias.OneExecution
 
 interface AuthView : MvpView {
 
     @AddToEndSingle
     fun showMessage(msg:String)
 
-    @AddToEndSingle
+    @OneExecution
     fun onLoginSuccess()
 
-    @AddToEndSingle
+    @OneExecution
     fun onLoginError()
 }
